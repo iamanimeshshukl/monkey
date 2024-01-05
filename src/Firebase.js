cfa226e8a3160effb64ec5ec26f2001ec389145a
Animesh shukla <75159346+iamanimeshshukl@users.noreply.github.com>
2024-01-05 23:18:29 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {GoogleAuthProvider, getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUvP6w0uGBnxJLZ1xwyZvDHPgxymWVcrs",
-  authDomain: "monkey-588de.firebaseapp.com",
-  projectId: "monkey-588de",
-  storageBucket: "monkey-588de.appspot.com",
-  messagingSenderId: "490814481077",
-  appId: "1:490814481077:web:110a721902c534e58a94fc",
-  measurementId: "G-HTD66Z8ZGJ"
+  apiKey: "your api",
+  authDomain: "your auth domain",
+  projectId: "your project id",
+  storageBucket: "your storage bucket",
+  messagingSenderId: "your messaging id",
+  appId: "your app id",
+  measurementId: "your measure id"
 };
 
 const app = initializeApp(firebaseConfig);
